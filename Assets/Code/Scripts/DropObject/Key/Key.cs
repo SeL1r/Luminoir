a6876a313b013f9c180826a146723e77c4044c31
SeL1r <froud2007@gmail.com>
2025-12-10 22:16:44 +0300

    AnimOpenDoor

--- Assets/Code/Scripts/DropObject/Key/Key.cs
+++ Assets/Code/Scripts/DropObject/Key/Key.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Key : ObjectWhichDrop, IActivate
@@ -11,8 +12,14 @@ public class Key : ObjectWhichDrop, IActivate
 		{
 			return;
 		}
-		myTriggerDoor.SetActive(false);
 		transform.position = new Vector3(1000, 1000, 1000);
-		
+		myTriggerDoor.SetActive(false);
+		StartCoroutine(OpenDoorWithDelay());
+	}
+	private IEnumerator OpenDoorWithDelay()
+	{
+		myDoor.GetComponent<Animator>().SetBool("IsUnLock", true);
+		yield return new WaitForSeconds(0.5f);
+		myDoor.GetComponent<Animator>().SetBool("IsUnLock", false);
 	}
 }
